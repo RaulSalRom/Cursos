5ace998a347c04dc9b97bcadeb21dbc4b1ab0202
RaulSalRom <salromeoraul@gmail.com>
2026-05-29 09:56:42 +0200

    puesta en practica apuntes hasta string

---
+++ Curso Javascript/ejemplos.js
@@ -0,0 +1,13 @@
+//mostrar información por consola("Devuelves al usuario")
+//enteros:
+consolelog(1);
+consolelog(21);
+consolelog(-16);
+//decimales:
+consolelog(3.14);
+consolelog(1.6);
+consolelog(-5.89);
+//string con comillas dobles:
+consolelog("Hola Fosela");
+//string con comillas simples:
+consolelog('Hola Fosela');
